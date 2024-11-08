7dd9e7d8f6fe50ca49caa7d063ecb3c79fea6ccc
santiago <santiagodeandrade4@gmail.com>
2024-11-08 16:32:05 -0400

    creacion de managers para mejorar consulta de datos de clientes

--- backend/applications/Clientes/managers.py
+++ backend/applications/Clientes/managers.py
@@ -3,3 +3,18 @@ from django.contrib.auth.models import BaseUserManager
 class ClientesManager(BaseUserManager):
     def crear_cliente(self, perfil):
         return self.model.objects.create(perfil=perfil)
+    def get_formated_direcciones_dict(self, direcciones):
+        formated_direcciones = []
+        BASE_DIRECCIONES_SHOWABLE_FIELDS = ["direccion_1", "direccion_2", "codigo_postal", "pais", "estado"]
+        for d in direcciones:
+            formated_direcciones.append(
+                {k:v for k,v in d.__dict__.items() if k in BASE_DIRECCIONES_SHOWABLE_FIELDS}
+            )
+        return formated_direcciones;
+    def get_client_info(self, cliente):
+        from applications.Perfiles.models import Perfiles
+        return {
+            'perfil' : Perfiles.get_perfil_dict(cliente.perfil),
+            'direcciones' : self.get_formated_direcciones_dict(cliente.direcciones.all()),
+        }
+        
