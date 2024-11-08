61f0201dd4d2801ffd9f0e714b62d8bcccf8d141
santiago <santiagodeandrade4@gmail.com>
2024-11-08 12:00:44 -0400

    Relacion entre producto y resenia ejecutada y modificacion de admin para que muestre: calificacion, cliente, y titulo por resenia

--- backend/applications/Productos/admin.py
+++ backend/applications/Productos/admin.py
@@ -11,9 +11,12 @@ class ProductosAdmin(admin.ModelAdmin):
 
 @admin.register(Resenias)
 class ReseniasAdmin(admin.ModelAdmin):  
-    list_display = ('calificacion', 'cliente_nombre_completo')  
+    list_display = ('calificacion', 'cliente_nombre_completo', 'titulo_producto')  
 
     def cliente_nombre_completo(self, obj):  
         return obj.cliente_asociado.perfil.nombre_completo  # Accede al cliente relacionado y su propiedad  
+    def titulo_producto(self, obj):
+        return obj.producto_asociado.titulo
     cliente_nombre_completo.short_description = 'Cliente'  # Título de la columna  
+    titulo_producto.short_description = 'Titulo del Producto'  # Título de la columna  
 
--- backend/applications/Productos/models.py
+++ backend/applications/Productos/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from applications.Clientes.models import Clientes
 
-# Create your models here.
-class Resenias(models.Model):
-    calificacion = models.IntegerField()
-    descripcion = models.TextField()
-    # con el campo related_name, podemos acceder a traves de una instancia de cliente a sus resenias asociadas
-    cliente_asociado = models.ForeignKey(Clientes, related_name="resenias", on_delete=models.CASCADE) 
-
 class Productos(models.Model):
     titulo      = models.CharField(unique=True)
     descripcion = models.TextField()
@@ -19,3 +12,19 @@ class Productos(models.Model):
     class Meta:
         verbose_name = 'Producto'
         verbose_name_plural = 'Productos'
+
+class Resenias(models.Model):
+    calificacion = models.IntegerField()
+    descripcion = models.TextField()
+    # con el campo related_name, podemos acceder a traves de una instancia de cliente a sus resenias asociadas
+    cliente_asociado = models.ForeignKey(Clientes, related_name="resenias", on_delete=models.CASCADE) 
+    producto_asociado = models.ForeignKey(Productos, related_name="resenias", on_delete=models.CASCADE) 
+
+    def __str__(self):
+        return f"{self.calificacion} : {self.descripcion}"
+    class Meta:
+        verbose_name = 'Resenia'
+        verbose_name_plural = 'Resenias'
+
+
+
