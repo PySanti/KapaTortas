27a8b0bdd7d7c7343105988ab1c4ad5502a22b96
santiago <santiagodeandrade4@gmail.com>
2024-11-08 12:31:30 -0400

    Creacion de model DireccionesEnvio

--- backend/applications/Clientes/admin.py
+++ backend/applications/Clientes/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin  
-from .models import Clientes
+from .models import (
+    Clientes,
+    DireccionesEnvio
+)
 
 admin.site.register(Clientes) 
+
+@admin.register(DireccionesEnvio)  
+class DireccionesEnvioAdmin(admin.ModelAdmin):  
+    list_display = ('id', 'pais', 'codigo_postal')  # Agrega los campos que deseas mostrar en el admin  
+
+
--- backend/applications/Clientes/models.py
+++ backend/applications/Clientes/models.py
@@ -3,7 +3,19 @@ from applications.Perfiles.models import Perfiles
 from .managers import ClientesManager
 
 
-# Create your models here.
+class DireccionesEnvio(models.Model):
+    direccion_1     = models.CharField()
+    direccion_2     = models.CharField()
+    pais            = models.CharField()
+    estado          = models.CharField()
+    codigo_postal   = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id} : {self.pais}/{self.codigo_postal}"
+    class Meta:
+        verbose_name = 'DirecciónEnvío'
+        verbose_name_plural = 'DireccionesEnvío'
+
 
 class Clientes(models.Model):  
     perfil          =   models.OneToOneField(Perfiles, on_delete=models.CASCADE)
@@ -18,3 +30,5 @@ class Clientes(models.Model):
     class Meta:
         verbose_name = 'Cliente'
         verbose_name_plural = 'Clientes'
+
+
