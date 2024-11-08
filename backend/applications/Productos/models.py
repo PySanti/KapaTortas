af39d08d2ce269cc38ee75f81d9d885a45a4b81f
santiago <santiagodeandrade4@gmail.com>
2024-11-08 12:18:50 -0400

    Adicion de campo 'imagenes' a la tabla Productos, un array de strings de maximo 200 caracteres

--- backend/applications/Productos/models.py
+++ backend/applications/Productos/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from applications.Clientes.models import Clientes
+from django.contrib.postgres.fields import ArrayField  
+
 
 class Productos(models.Model):
     titulo      = models.CharField(unique=True)
     descripcion = models.TextField()
     precio      = models.DecimalField(max_digits=5,decimal_places=2)
     stock       = models.IntegerField()
+    imagenes    = ArrayField(models.CharField(max_length=200), blank=True, default=list)  
 
     def __str__(self):
         return f"{self.titulo} : {self.stock}"
