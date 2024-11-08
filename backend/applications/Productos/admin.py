7cd13f0e848b1b82cf2d0ead7dac2c78e93705a5
santiago <santiagodeandrade4@gmail.com>
2024-11-08 11:49:20 -0400

    Relacion entre resenias y clientes ejecutada, y registro de resenias en el administrador

--- backend/applications/Productos/admin.py
+++ backend/applications/Productos/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin  
-from .models import Productos
+from .models import (
+    Productos,
+    Resenias
+)
 
 @admin.register(Productos)  
 class ProductosAdmin(admin.ModelAdmin):  
     list_display = ('titulo', 'stock')  # Agrega los campos que deseas mostrar en el admin  
+
+
+@admin.register(Resenias)
+class ReseniasAdmin(admin.ModelAdmin):  
+    list_display = ('calificacion', 'cliente_nombre_completo')  
+
+    def cliente_nombre_completo(self, obj):  
+        return obj.cliente_asociado.perfil.nombre_completo  # Accede al cliente relacionado y su propiedad  
+    cliente_nombre_completo.short_description = 'Cliente'  # Título de la columna  
 
--- backend/applications/Productos/models.py
+++ backend/applications/Productos/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from applications.Clientes.models import Clientes
 
 # Create your models here.
-class Resenia(models.Model):
+class Resenias(models.Model):
     calificacion = models.IntegerField()
     descripcion = models.TextField()
+    # con el campo related_name, podemos acceder a traves de una instancia de cliente a sus resenias asociadas
+    cliente_asociado = models.ForeignKey(Clientes, related_name="resenias", on_delete=models.CASCADE) 
 
 class Productos(models.Model):
     titulo      = models.CharField(unique=True)
