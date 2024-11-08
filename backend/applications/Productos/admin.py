14e8bfd65e557041634d393b56b5963dfd67ced3
santiago <santiagodeandrade4@gmail.com>
2024-11-08 10:43:37 -0400

    Adicion de campos a tabla Productos

--- backend/applications/Productos/admin.py
+++ backend/applications/Productos/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin  
 from .models import Productos
 
-admin.site.register(Productos) 
+@admin.register(Productos)  
+class ProductosAdmin(admin.ModelAdmin):  
+    list_display = ('titulo', 'stock')  # Agrega los campos que deseas mostrar en el admin  
+
--- backend/applications/Productos/models.py
+++ backend/applications/Productos/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
 class Productos(models.Model):
-    pass
+    titulo      = models.CharField()
+    descripcion = models.TextField()
+    precio      = models.FloatField()
+    stock       = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.titulo} : {self.stock}"
+    class Meta:
+        verbose_name = 'Producto'
+        verbose_name_plural = 'Productos'
