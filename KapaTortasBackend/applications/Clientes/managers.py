e5591f31fba4cbd4f68147625d642bb3495f2099
santiago <santiagodeandrade4@gmail.com>
2024-11-08 10:25:15 -0400

    Modificacion de nombre de proyecto: KapaTortasBackend -> backend

--- KapaTortasBackend/applications/Clientes/managers.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.models import BaseUserManager
-
-class ClientesManager(BaseUserManager):
-    def crear_cliente(self, perfil):
-        return self.model.objects.create(perfil=perfil)
