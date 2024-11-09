c494b6d0c9089082ed19a315d6d255a4b0290dfa
santiago <santiagodeandrade4@gmail.com>
2024-11-09 12:06:41 -0400

    Mejora de muestra de ventas en administrador

--- backend/applications/Ventas/admin.py
+++ backend/applications/Ventas/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Ventas
 
-# Register your models here.
-admin.site.register(Ventas)
+@admin.register(Ventas)  
+class VentasAdmin(admin.ModelAdmin):  
+    list_display = ('numero_de_orden','fecha',  'monto_total', 'direccion', 'productos')  # Agrega los campos que deseas mostrar en el admin  
+    def productos(self, obj):  
+        productos_str = []
+        for p in obj.productos_asociados.all():
+            productos_str.append(f"{p.titulo}")
+        return "-".join(productos_str) # Accede al cliente relacionado y su propiedad  
+    productos.short_description = 'productos'  # Título de la columna  
+
