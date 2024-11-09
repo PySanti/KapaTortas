837d368e863853b644e8007e91a0fa03c509101c
Samuel Palacios <samuelpl0888@gmail.com>
2024-11-09 11:45:24 -0400

    add: mvc view

--- frontend/app/views/pages/Homepage.tsx
+++ frontend/app/views/pages/Homepage.tsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import KapaTortasVerifyEmail from "../components/verification-email";
-import { Perfil } from "@/app/models/Perfil";
-import PerfilAPI from "@/app/controladores/api/users/PerfilAPI";
+import { useState } from 'react';
+import KapaTortasVerifyEmail from '../components/verification-email';
+import { Perfil } from '@/app/models/Perfil';
+import PerfilAPI from '@/app/controladores/api/users/PerfilAPI';
 
 export default function HomePage({}) {
   return (
@@ -17,7 +17,7 @@ export default function HomePage({}) {
 // Esto esta malo, deberiamos traer el Perfil en contexto, es para testear.
 const PerfilTesteo: React.FC = () => {
   const [perfil, setPerfil] = useState<Perfil | null>(null);
-  const [email, setEmail] = useState<string>("");
+  const [email, setEmail] = useState<string>('');
   const [error, setError] = useState<string | null>(null);
 
   const handleFetchPerfil = async () => {
@@ -27,28 +27,25 @@ const PerfilTesteo: React.FC = () => {
         setPerfil(fetchPerfil);
         setError(null);
       } else {
-        setError("Perfil no encontrado");
+        setError('Perfil no encontrado');
       }
     } catch (err) {
-      console.error("Error intentando filtrar perfil: ", err);
-      setError("Hubo un problema al consultar el perfil.");
+      console.error('Error intentando filtrar perfil: ', err);
+      setError('Hubo un problema al consultar el perfil.');
     }
   };
 
   return (
     <>
-      <h2 className="text-2xl p-20">Testing del perfil</h2>
-      <div className="p-5">
+      <h2 className='text-2xl p-20'>Testing del perfil</h2>
+      <div className='p-5'>
         <input
-          type="text"
-          placeholder="Pon el texto"
+          type='text'
+          placeholder='Pon el texto'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <button
-          className="bg-green-300 p-5 rounded-lg "
-          onClick={handleFetchPerfil}
-        >
+        <button className='bg-green-300 p-5 rounded-lg ' onClick={handleFetchPerfil}>
           Consulta el perfil
         </button>
       </div>
@@ -56,7 +53,7 @@ const PerfilTesteo: React.FC = () => {
       {error && <p>{error}</p>}
 
       {perfil ? (
-        <div className="p-20 text-sm">
+        <div className='p-20 text-sm'>
           <h2>Perfil de {perfil.nombre_completo}</h2>
           <h2>Correo: {perfil.correo}</h2>
         </div>
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '**',
+      },
+
+      {
+        protocol: 'https',
+        hostname: 'kapatortas.com',
+        port: '',
+        pathname: '**',
+      },
+
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
