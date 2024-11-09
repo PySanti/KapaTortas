d2dad237ae5eea1b5c3590423f7ae33a4ed251ad
santiago <santiagodeandrade4@gmail.com>
2024-11-09 17:05:09 -0400

    Adicion de campo 'contrasenia' al retorno de ConsultarPerfilAPI

--- backend/backend/utils/constants.py
+++ backend/backend/utils/constants.py
@@ -9,7 +9,7 @@ class RolEnum(Enum):
 
 
 BASE_SERIALIZER_ERROR_RESPONSE = Response({'error' : "serializer_error"}, status.HTTP_400_BAD_REQUEST)
-BASE_PROFILE_SHOWABLE_FIELDS = ("nombre_completo", "numero_telefonico", "fecha_nacimiento", "correo", "id", "link_foto", "rol")
+BASE_PROFILE_SHOWABLE_FIELDS = ("nombre_completo", "numero_telefonico", "fecha_nacimiento", "correo", "id", "link_foto", "rol", "contraseña")
 BASE_DIRECCIONES_SHOWABLE_FIELDS = ["direccion_1", "direccion_2", "codigo_postal", "pais", "estado"]
 BASE_PRODUCTOS_LIST_SHOWABLE_FIELDS = ["id", "titulo", "stock", "imagenes"]
 BASE_REVIEWS_SHOWABLE_FIELDS = ["calificacion", "descripcion", ]
