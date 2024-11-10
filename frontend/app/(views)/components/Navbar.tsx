9b5a87d1bf4f0d9b0fe84f9122ee3d5fbdca3090
danirssx <rossdaniel5018@gmail.com>
2024-11-10 10:12:27 -0400

    Changes in Homepage

--- frontend/app/(views)/components/Navbar.tsx
+++ frontend/app/(views)/components/Navbar.tsx
@@ -1,38 +1,35 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-import { Menu } from "lucide-react"
-import { Button } from "@/components/ui/button"
-import { MainButton } from "./MainButton"
+import Link from "next/link";
+import { Menu } from "lucide-react";
+import { MainButton } from "./MainButton";
 
-import Logo from "@/components/images/Logo"
-
+import Logo from "@/components/images/Logo";
 
 export default function Navbar() {
-    return (
-        <header className="sticky top-0 z-50 w-full border-b bg-secondary">
-            <div className="grid grid-cols-3 h-25 items-center px-6 sm:px-8 md:px-12 lg:px-16">
-                <div className="flex items-center space-x-2">
-                    <Menu className="h-6 w-6 lg:h-8 lg:w-8 text-primary" />
-                    <h3 className="text-lg lg:text-2xl text-primary">Menú</h3>
-                </div>
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-secondary">
+      <div className="grid grid-cols-3 h-25 items-center px-6 sm:px-8 md:px-12 lg:px-16">
+        <div className="flex items-center space-x-2">
+          <Menu className="h-6 w-6 lg:h-8 lg:w-8 text-primary" />
+          <h3 className="text-lg lg:text-2xl text-primary">Menú</h3>
+        </div>
 
-                <div className="flex justify-center">
-                    <Link href="/">
-                        <Logo />
-                    </Link>
-                </div>
+        <div className="flex justify-center">
+          <Link href="/">
+            <Logo />
+          </Link>
+        </div>
 
-               
-                <div className="hidden lg:flex justify-end items-center space-x-4">
-                    <Link href="/catalogo" passHref>
-                        <MainButton variant="secondary">Realiza tu Pedido</MainButton>
-                    </Link>
-                    <Link href="/registro">
-                        <MainButton>Registrarse</MainButton>
-                    </Link> 
-                </div>
-            </div>
-        </header>
-    )
+        <div className="hidden lg:flex justify-end items-center space-x-4">
+          <Link href="/catalogo" passHref>
+            <MainButton variant="secondary">Realiza tu Pedido</MainButton>
+          </Link>
+          <Link href="/registro">
+            <MainButton>Registrarse</MainButton>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
--- frontend/components/images/Torta.tsx
+++ frontend/components/images/Torta.tsx
@@ -1,15 +1,19 @@
 import Image from "next/image";
 
-export default function Torta({ path = "", alt = "", className }: {path: string, alt: string, className?: string}) {
-    return (
-        <div className={`${className} -ml-20 w-[60vw] xs:w-[50vw] xs:-ml-24 sm:w-[45vw] md:-ml-24 md:w-[40vw] lg:-ml-24 xl:ml-0  lg:w-[40vw]`}>
-            <Image
-                src={path}
-                alt={ alt }
-                width={550}
-                height={550}
-            />   
-        </div>
-         
-    )
+export default function Torta({
+  path = "",
+  alt = "",
+  className,
+}: {
+  path: string;
+  alt: string;
+  className?: string;
+}) {
+  return (
+    <div
+      className={`${className} -ml-20 w-[60vw] xs:w-[50vw] xs:-ml-24 sm:w-[40vw] md:-ml-24 md:w-[40vw] lg:-ml-24 xl:ml-0  lg:w-[40vw]`}
+    >
+      <Image src={path} alt={alt} width={550} height={550} />
+    </div>
+  );
 }
