f08e149f184479da7f441cceffb8e31695ee9db5
Samuel Palacios <samuelpl0888@gmail.com>
2024-11-11 10:23:33 -0400

    update: page orders setup

--- frontend/app/(views)/dashboard/pedidos/page.tsx
+++ frontend/app/(views)/dashboard/pedidos/page.tsx
@@ -1,5 +1,6 @@
 import DashboardContainer from '../../components/dashboard-container';
 import { DashboardHeader } from '../../components/dashboard-header';
+import PedidoHistorial from '../../components/orden-historial';
 
 export default function OrdenesPage() {
   return (
@@ -8,6 +9,7 @@ export default function OrdenesPage() {
         heading='Pedidos'
         description='Revisa el estado de tus pedidos y realiza cambios si es necesario'
       />
+      <PedidoHistorial />
     </DashboardContainer>
   );
 }
