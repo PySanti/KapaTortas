fe6c0bf442595fc3b7b77c705c41f5a54ec290a0
danirssx <rossdaniel5018@gmail.com>
2024-11-11 14:37:42 -0400

    Layout product fixed

--- frontend/app/(views)/product/layout.tsx
+++ frontend/app/(views)/product/layout.tsx
@@ -2,15 +2,19 @@ import Navbar from "../components/main-nav";
 import Footer from "../components/footer";
 
 export default async function ProductLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <div className="min-h-screen flex flex-col relative">
-          <Navbar />
-          <main className='flex-grow pb-[footer-height] relative z-0'>{children}</main>
-          <Footer />
-        </div>
-    )
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header className="sticky top-0 bg-gray-50 shadow inset-x-0 z-[50]">
+        <Navbar className="max-w-none" />
+      </header>
+      <main className="flex-grow pb-[footer-height] relative z-0">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
 }
