d25583eeedf0338712a2bcf303d99e9d6460f6cf
danirssx <rossdaniel5018@gmail.com>
2024-11-11 12:56:38 -0400

    Images

--- frontend/components/images/Eyes.tsx
+++ frontend/components/images/Eyes.tsx
@@ -1,19 +1,18 @@
 import Image from "next/image";
 
-import homePhoto from "@/public/images/eyes-1.png"
+import eyes from "@/public/images/eyes-1.png";
 
 export default function Eyes() {
-    return (
-        <div className="w-[20vw] md:w-[15vw] lg:w-[10vw] xl:w-[7vw] transform -rotate-12">
-            <Image
-                src={homePhoto}
-                alt="Logo"
-                layout="responsive"
-                width={100}
-                height={100}
-                priority
-            />   
-        </div>
-         
-    )
+  return (
+    <div className="w-[20vw] md:w-[15vw] lg:w-[10vw] xl:w-[7vw] transform -rotate-12">
+      <Image
+        src={eyes}
+        alt="Logo"
+        width={100}
+        height={100}
+        style={{ width: "100%", height: "auto" }}
+        priority
+      />
+    </div>
+  );
 }
--- frontend/components/images/HomePhoto.tsx
+++ frontend/components/images/HomePhoto.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 
-import homePhoto from "@/public/images/Home-Photo.png"
+import homePhoto from "@/public/images/Home-Photo.png";
 
 export default function HomePhoto() {
-    return (
-        <Image
-            src={homePhoto}
-            alt="Logo"
-            layout="fill"
-            objectFit="cover"
-            priority
-        />    
-    )
+  return (
+    <Image
+      src={homePhoto}
+      alt="Logo"
+      style={{ objectFit: "cover" }}
+      fill
+      priority
+    />
+  );
 }
