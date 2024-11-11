bd9d51f3c80bc2a3b42d98aa2bca0ab401a526d7
santiago <santiagodeandrade4@gmail.com>
2024-11-11 10:04:53 -0400

    Implementacion de configuraciones para envio de correo desde django

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,3 +125,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# email config
+
+
+
+EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST          = 'smtp.gmail.com'
+EMAIL_PORT          = 587
+EMAIL_USE_TLS       = True
+EMAIL_HOST_USER     = "kapatortas@gmail.com"
+EMAIL_HOST_PASSWORD = secret_data['EMAIL_PASSWORD']
+DEFAULT_FROM_EMAIL = 'kapatortas@gmail.com'  # El correo electrónico que se usará como remitente por defecto  
+
