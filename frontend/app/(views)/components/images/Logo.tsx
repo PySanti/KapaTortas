7bac7069b817d7c8246090ab2681143c34f4f0d4
Samuel Palacios <samuelpl0888@gmail.com>
2024-11-12 12:03:49 -0400

    update: navbar cambiar size y botones

--- frontend/app/(views)/components/images/Logo.tsx
+++ frontend/app/(views)/components/images/Logo.tsx
@@ -1,15 +1,7 @@
-import Image from "next/image";
+import Image from 'next/image';
 
-import logo from "@/public/images/Orange.png"
+import logo from '@/public/images/Orange.png';
 
 export default function Logo() {
-    return (
-        <Image
-            src={logo}
-            alt="Logo"
-            width={150}
-            height={150}
-            priority
-        />    
-    )
+  return <Image src={logo} alt='Logo' width={120} height={120} priority />;
 }
