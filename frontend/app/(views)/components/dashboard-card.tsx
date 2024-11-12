2f1600d8251d90b0c23076994decd50bb5a31f4d
Samuel Palacios <samuelpl0888@gmail.com>
2024-11-12 16:32:41 -0400

    update: refactorizacion dashboard card

---
+++ frontend/app/(views)/components/dashboard-card.tsx
@@ -0,0 +1,55 @@
+import { Card, CardContent, CardFooter, CardHeader } from '@/app/(views)/components/ui/card';
+import { Separator } from '@/app/(views)/components/ui/separator';
+import { Button } from '@/app/(views)/components/ui/button';
+import { Trash } from 'lucide-react';
+
+interface DashboardCardProps extends React.HTMLAttributes<HTMLDivElement> {
+  title?: string; // Optional titulo para el header del card
+  highlight?: string; // Optional highlight titulo (e.g., "Dirección preferido")
+  children: React.ReactNode; // Contenido del card
+  editable?: boolean; //  Optional Activa el botón de editar
+  deletable?: boolean; // Optional Activa el botón de eliminar
+}
+
+export default function DashboardCard({
+  title,
+  highlight,
+  editable,
+  deletable,
+  children,
+  className,
+  ...props
+}: DashboardCardProps) {
+  return (
+    <Card className={`max-w-xl ${className}`} {...props}>
+      <CardHeader>
+        {highlight && (
+          <>
+            <div className='max-w-fit p-1 bg-gray-200 rounded-md'>
+              <p className='text-sm text-terciary'>{highlight}</p>
+            </div>
+            <Separator />
+          </>
+        )}
+        {title && (
+          <>
+            <h3 className='text-lg text-terciary font-medium'>{title}</h3>
+            <Separator />
+          </>
+        )}
+      </CardHeader>
+
+      <CardContent className='space-y-2'>{children}</CardContent>
+      <CardFooter className='flex items-center gap-x-2'>
+        {editable && (
+          <Button className='bg-white text-terciary hover:bg-gray-50 border-2'>Editar</Button>
+        )}
+        {deletable && (
+          <Button className='bg-white text-terciary hover:bg-gray-50 border-2'>
+            <Trash />
+          </Button>
+        )}
+      </CardFooter>
+    </Card>
+  );
+}
--- frontend/app/(views)/dashboard/direcciones/page.tsx
+++ frontend/app/(views)/dashboard/direcciones/page.tsx
@@ -1,7 +1,8 @@
 import { DireccionEnvio } from '@/app/models/Cliente';
 import DashboardContainer from '../../components/dashboard-container';
 import { DashboardHeader } from '../../components/dashboard-header';
-import DireccionEnvioCard from '../../components/direccion-envio-card';
+import DireccionEnvioCard from '../../components/dashboard-card';
+import DashboardCard from '../../components/dashboard-card';
 
 const sampleAddress: DireccionEnvio = {
   ciudad: 'Montreal',
@@ -40,36 +41,26 @@ export default function DireccionesPage() {
         heading='Direcciones'
         description='Edita, elimina o establece una dirección de envío predeterminada'
       />
-      <DireccionEnvioCard
-        nombreCompleto={cliente.nombre_completo}
-        direccion={sampleAddress}
-        numeroTelefono={cliente.numeroTelefono}
-      />
-      <DireccionEnvioCard
-        nombreCompleto={cliente2.nombre_completo}
-        direccion={sampleAddress2}
-        numeroTelefono={cliente2.numeroTelefono}
-      />
-      <DireccionEnvioCard
-        nombreCompleto={cliente.nombre_completo}
-        direccion={sampleAddress}
-        numeroTelefono={cliente.numeroTelefono}
-      />
-      <DireccionEnvioCard
-        nombreCompleto={cliente.nombre_completo}
-        direccion={sampleAddress}
-        numeroTelefono={cliente.numeroTelefono}
-      />
-      <DireccionEnvioCard
-        nombreCompleto={cliente.nombre_completo}
-        direccion={sampleAddress}
-        numeroTelefono={cliente.numeroTelefono}
-      />
-      <DireccionEnvioCard
-        nombreCompleto={cliente.nombre_completo}
-        direccion={sampleAddress}
-        numeroTelefono={cliente.numeroTelefono}
-      />
+      <DashboardCard highlight='Dirección de envío preferida' editable deletable>
+        <p className='text-terciary text-sm'>{cliente.nombre_completo}</p>
+        <p className='text-terciary text-sm'>{`${sampleAddress.direccion} ${sampleAddress.referencia}, ${sampleAddress.ciudad}, ${sampleAddress.estado}, ${sampleAddress.codigo_postal}`}</p>
+        <p className='text-terciary text-sm'>{cliente.numeroTelefono}</p>
+      </DashboardCard>
+      <DashboardCard editable deletable>
+        <p className='text-terciary text-sm'>{cliente2.nombre_completo}</p>
+        <p className='text-terciary text-sm'>{`${sampleAddress2.direccion} ${sampleAddress2.referencia}, ${sampleAddress2.ciudad}, ${sampleAddress2.estado}, ${sampleAddress2.codigo_postal}`}</p>
+        <p className='text-terciary text-sm'>{cliente2.numeroTelefono}</p>
+      </DashboardCard>
+      <DashboardCard editable deletable>
+        <p className='text-terciary text-sm'>{cliente.nombre_completo}</p>
+        <p className='text-terciary text-sm'>{`${sampleAddress.direccion} ${sampleAddress.referencia}, ${sampleAddress.ciudad}, ${sampleAddress.estado}, ${sampleAddress.codigo_postal}`}</p>
+        <p className='text-terciary text-sm'>{cliente.numeroTelefono}</p>
+      </DashboardCard>
+      <DashboardCard editable deletable>
+        <p className='text-terciary text-sm'>{cliente2.nombre_completo}</p>
+        <p className='text-terciary text-sm'>{`${sampleAddress2.direccion} ${sampleAddress2.referencia}, ${sampleAddress2.ciudad}, ${sampleAddress2.estado}, ${sampleAddress2.codigo_postal}`}</p>
+        <p className='text-terciary text-sm'>{cliente2.numeroTelefono}</p>
+      </DashboardCard>
     </DashboardContainer>
   );
 }
