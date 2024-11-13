634d0fd60db46f0e5d533520f9ec21bb24f51fc6
danirssx <rossdaniel5018@gmail.com>
2024-11-13 18:05:45 -0400

    ruta pedido

---
+++ frontend/app/(views)/pedido/caja/page.tsx
@@ -0,0 +1,9 @@
+import MainPedido from "../../components/main-pedido";
+
+export default async function PedidoPage() {
+    return (
+        <div>
+            <MainPedido />
+        </div>
+    )
+}
