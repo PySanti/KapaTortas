66c53d0340feb329c84c682fc2f96d46f98e9739
Samuel Palacios <samuelpl0888@gmail.com>
2024-11-13 20:50:42 -0400

    update: dashboard responsive y con auth integrado

--- frontend/app/models/config/routes.ts
+++ frontend/app/models/config/routes.ts
@@ -1,10 +1,10 @@
 //  * Users can navigate to these routes regardless of their login status.
-export const publicRoutes = ["/products/", "/verify-email", "/pedido/"];
+export const publicRoutes = ['/products/', '/verify-email', '/pedido/'];
 
 // * Users if logged in and try to navigate to these routes, they will be redirected to the defaultLoginRedirect.
-export const authRoutes = ["/login", "/register"];
+export const authRoutes = ['/login', '/register'];
 // export const authRoutes = ['/login', '/register', '/reset-password', '/new-password'];
 
-export const defaultLoginRedirect = "/dashboard";
+export const defaultLoginRedirect = '/dashboard/ajustes';
 
 //* Protected routes are those that require the user to be logged in, this is managed on auth.config.ts
