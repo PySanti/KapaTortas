d42a68e5b29e4d8cae19b79c027c1bb5547ccc19
Samuel Palacios <samuelpl0888@gmail.com>
2024-12-31 14:54:31 -0400

    ref: now register form is multistep

--- frontend/app/controladores/lib/validations/auth.ts
+++ frontend/app/controladores/lib/validations/auth.ts
@@ -4,14 +4,26 @@ export const registroSchema = z
   .object({
     nombre: z
       .string()
-      .min(5, { message: 'Por favor, ingresa nombre y apellido' })
-      .regex(/^[^0-9]+$/, { message: 'El nombre no puede tener números' }),
+      .min(5, { message: 'El nombre debe tener al menos 5 caractéres' })
+      .max(50, { message: 'El nombre no puede tener más de 50 caractéres' })
+      .transform((value) => value.replace(/\s+/g, ' ')) // Normalize multiple spaces to one
+      .refine((value) => /^[A-Za-zÑñÁÉÍÓÚáéíóú]+ [A-Za-zÑñÁÉÍÓÚáéíóú]+$/.test(value), {
+        message: 'Por favor, ingresa nombre y apellido',
+      }),
+    // .regex(
+    //   /^[A-Za-zÑñÁÉÍÓÚáéíóú]+ [A-Za-zÑñÁÉÍÓÚáéíóú]+$/,
+    //   'Por favor, ingresa nombre y apellido'
+    // )
+    // .transform((value) => value.trim().replace(/\s+/g, ' ')), // Quita espacios redundantes
     email: z.string().email({ message: 'Por favor, ingresa un email válido' }),
     password: z.string().min(6, { message: 'La contraseña debe tener al menos 6 caractéres' }),
     confirmPassword: z
       .string()
       .min(6, { message: 'La contraseña debe tener al menos 6 caracteres' }),
     rol: z.enum(['cliente', 'empleado', 'admin']).optional(),
+    cedula: z.string().regex(/^[VE]-?\d{7,8}$/, {
+      message: 'La cédula debe comenzar con V o E y tener 7 u 8 dígitos',
+    }),
   })
   .refine(
     (form) => {
