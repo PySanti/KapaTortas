13761edeee33830468c9d81dc0726195a103387b
santiago <santiagodeandrade4@gmail.com>
2025-01-01 17:16:45 -0400

    Creacion de modelo Facturas

--- backend/applications/Ventas/models/models.py
+++ backend/applications/Ventas/models/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from backend.utils.constants import (DEFAULT_DOMICILIO_FISCAL, DEFAULT_CORREO_EMPRESA, DEFAULT_NUMERO_TELEFONICO_EMPRESA, DEFAULT_RIF_EMPRESA)
 from applications.Productos.models import (
     Presentaciones
 )
@@ -67,3 +68,11 @@ class Ventas(models.Model):
     class Meta:
         verbose_name = 'Venta'
         verbose_name_plural = 'Ventas'
+
+class Facturas(models.Model):
+    fecha_emision_factura               = models.DateTimeField()
+    venta_asociada                      = models.OneToOneField(Ventas, on_delete=models.SET_NULL, null=True, related_name="factura")
+    domicilio_fiscal                    = models.CharField(blank=True, default=DEFAULT_DOMICILIO_FISCAL)
+    numero_telefonico_empresa           = models.CharField(blank=True, default=DEFAULT_NUMERO_TELEFONICO_EMPRESA)
+    rif_empresa                         = models.CharField(blank=True, default=DEFAULT_RIF_EMPRESA)
+    correo_electronico_empresa          = models.EmailField(blank=True, default=DEFAULT_CORREO_EMPRESA)
