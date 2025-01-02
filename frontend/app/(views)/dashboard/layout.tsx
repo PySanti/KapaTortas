8eaae59a921c8807b222953e61d090fe6be5ee6d
Samuel Palacios <samuelpl0888@gmail.com>
2025-01-02 18:14:25 -0400

    update: role based navs

--- frontend/app/(views)/dashboard/layout.tsx
+++ frontend/app/(views)/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import { auth } from '@/auth';
 import { dashboardConfig, dashboardConfigEmpleado } from '@/app/models/config/dashboard';
+import { marketingConfig } from '@/app/models/config/marketing';
 import Navbar from '../components/main-nav';
 import Sidebar from '../components/sidebar';
 import { SessionProvider } from 'next-auth/react';
@@ -17,13 +18,13 @@ export default async function DashboardLayout({
     <SessionProvider session={session}>
       <div className='flex flex-col min-h-screen'>
         <header className='sticky top-0 bg-gray-50 shadow inset-x-0 z-[50]'>
-          {/* <Navbar className='max-w-none' user={user} /> */}
-          <Navbar className='max-w-none' user={user} items={dashboardConfig.sidebarNav} />
+          <Navbar className='max-w-none' user={user} items={marketingConfig.mainNav} />
         </header>
 
         <div className='container flex-1 items-start md:grid md:grid-cols-[160px_minmax(0,1fr)] md:gap-2 lg:grid-cols-[200px_minmax(0,1fr)] lg:gap-4'>
           <aside className='fixed top-14 pt-6 lg:pt-10 z-40 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block'>
             <div className='h-full pr-6'>
+              {/* Role based sidebar */}
               {user?.rol === Rol.CLIENTE ? (
                 <Sidebar items={dashboardConfig.sidebarNav} />
               ) : (
