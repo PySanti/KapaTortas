1151c6240632ba66d28ae8306cb24efa8a139114
Samuel Palacios <samuelpl0888@gmail.com>
2025-01-04 16:47:00 -0400

    add: endpoint editar producto by id

--- backend/applications/Productos/serializers/serializers.py
+++ backend/applications/Productos/serializers/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
+from backend.utils.constants import CategoriaProductoEnum
 
 class ConsultarProductoSerializer(serializers.Serializer):
     pass
 
 class ObtenerListaProductosSerializer(serializers.Serializer):
     pass
+
+class EditarProductoByIdSerializer(serializers.Serializer):
+    new_titulo = serializers.CharField(allow_null=True)
+    new_categoria = serializers.ChoiceField(
+        choices=[(categoria.value, categoria.name) for categoria in CategoriaProductoEnum],
+        allow_null=True
+    )
+    new_descripcion = serializers.CharField(allow_null=True)
+    new_imagenes = serializers.ListField(
+        child=serializers.CharField(max_length=200),
+        allow_null=True
+    )
 
 
 
--- backend/applications/Productos/urls.py
+++ backend/applications/Productos/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .controllers.views import (
     ConsultarProductoAPI,
-    ObtenerListaProductosAPI
+    ObtenerListaProductosAPI,
+    EditarProductoByIdAPI
 )
 
 urlpatterns = [
     path("all_productos/",ObtenerListaProductosAPI.as_view()),
     path("<int:id_producto>/",ConsultarProductoAPI.as_view()),
+    path("editar/<int:id_producto>/",EditarProductoByIdAPI.as_view()),
 ]
