c095f6e2f7cf3603d62ef28f72e95e59a28d34a6
Samuel Palacios <samuelpl0888@gmail.com>
2025-01-04 20:10:20 -0400

    update: helper functions

--- frontend/app/controladores/utilities/transform-estado-pedido.tsx
+++ frontend/app/controladores/utilities/transform-estado-pedido.tsx
@@ -1,6 +1,6 @@
 import { EstadoEnum } from "@/app/models/Pedido";
 
-const estados = [
+export const estadosPedido = [
   { label: "Recibido", value: EstadoEnum.RECIBIDO },
   { label: "En proceso", value: EstadoEnum.EN_PROCESO },
   { label: "Cancelado", value: EstadoEnum.CANCELADO },
@@ -9,6 +9,6 @@ const estados = [
 
 // Helper function to transform the estado field to a readable string
 export const transformEstadoPedido = (estadoInput: EstadoEnum): string => {
-  const estado = estados.find((estado) => estado.value === estadoInput);
+  const estado = estadosPedido.find((estado) => estado.value === estadoInput);
   return estado ? estado.label : "Estado no disponible";
 };
--- frontend/app/controladores/utilities/transform-metodo-pago.tsx
+++ frontend/app/controladores/utilities/transform-metodo-pago.tsx
@@ -1,12 +1,12 @@
 import { MetodoPago } from "@/app/models/Pedido";
 
-const metodoPagoArray = [
+const metodosDePago = [
   { metodo: MetodoPago.ZELLE, label: "Zelle" },
   { metodo: MetodoPago.PAGO_MOVIL, label: "Pago Móvil" },
 ];
 
 // Helper function to transform the metodo_pago field to a readable string
 export const transformMetodoPago = (metodoPago: MetodoPago): string => {
-  const metodo = metodoPagoArray.find((metodo) => metodo.metodo === metodoPago);
+  const metodo = metodosDePago.find((metodo) => metodo.metodo === metodoPago);
   return metodo ? metodo.label : "Método de pago no disponible";
 };
