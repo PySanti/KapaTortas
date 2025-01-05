c4b419de984d4e89dc154ca6af3b152d20e65980
Samuel Palacios <samuelpl0888@gmail.com>
2025-01-05 05:32:30 -0400

    update: refactoring styles

--- frontend/app/(views)/components/display-on-hover.tsx
+++ frontend/app/(views)/components/display-on-hover.tsx
@@ -4,20 +4,30 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/app/(views)/components/ui/tooltip";
+import { cn } from "@/app/controladores/lib/utils";
 import { ChevronDown } from "lucide-react";
 
 interface DisplayOnHoverProps extends React.HTMLAttributes<HTMLDivElement> {
   mainInfo: string;
   extraInfo: string[];
   className?: string;
+  displayMainInfo?: boolean;
 }
 
-export default function DisplayOnHover({ mainInfo, extraInfo, className }: DisplayOnHoverProps) {
+export default function DisplayOnHover({
+  mainInfo,
+  extraInfo,
+  className,
+  displayMainInfo = true, // Muestra el titulo (mainInfo) en el tooltip
+}: DisplayOnHoverProps) {
   return (
     <TooltipProvider delayDuration={300}>
       <Tooltip delayDuration={300}>
         <div
-          className={`relative flex flex-row items-center space-x-1 cursor-pointer text-terciary-muted ${className}`}
+          className={cn(
+            "relative flex flex-row items-center space-x-1 cursor-pointer text-terciary-muted",
+            className
+          )}
         >
           <TooltipTrigger className="flex hover:text-terciary transition-colors">
             {mainInfo}
@@ -25,7 +35,7 @@ export default function DisplayOnHover({ mainInfo, extraInfo, className }: Displ
           </TooltipTrigger>
         </div>
         <TooltipContent className="max-w-56" side="bottom">
-          <p className="text-sm font-bold text-terciary">{mainInfo}</p>
+          {displayMainInfo && <p className="text-sm font-bold text-terciary">{mainInfo}</p>}
           {extraInfo.map((info, index) => (
             <p key={index} className="text-sm text-terciary">
               {info}
